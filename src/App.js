2901424d5b2c1f63627545658d0dd7bee40c94e7
c2ireton <c2ireton@gmail.com>
2021-10-20 10:38:35 -0400

    changed what home page said

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          made change to see what would happen!		
         </p>
         <a
           className="App-link"
