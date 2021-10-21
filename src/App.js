120b8b188509286e9aae1f43ed51a7850a1a4864
anamsiddiqui0805 <92879186+anamsiddiqui0805@users.noreply.github.com>
2021-10-21 15:53:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,41 +3,94 @@ import React, { useState } from 'react';
 export default function App() {
 	const questions = [
 		{
-			questionText: 'What is the capital of France?',
+			questionText: 'Which service provides a user the ability to warehouse data in the AWS Cloud?',
 			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
+				{ answerText: 'Amazon EFS', isCorrect: false },
+				{ answerText: 'Amazon Redshift', isCorrect: true },
+				{ answerText: 'Amazon RDS', isCorrect: false },
+				{ answerText: 'Amazon VPC', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'Who is CEO of Tesla?',
+			questionText: 'Which AWS service can be used in the application deployment process?',
 			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
+				{ answerText: 'AWS AppSync', isCorrect: false },
+				{ answerText: 'AWS Batch', isCorrect: false },
+				{ answerText: 'AWS CodePipeline', isCorrect: true },
+				{ answerText: 'AWS DataSync', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'The iPhone was created by which company?',
+			questionText: 'True or False: AWS OpsWorks service is used to automate configuration management using Chef and Puppet.',
 			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
+				{ answerText: 'True', isCorrect: true },
+				{ answerText: 'False', isCorrect: false },	
 			],
 		},
 		{
-			questionText: 'How many Harry Potter books are there?',
+			questionText: 'Which AWS Cloud service can send alerts to customers if custom spending thresholds are exceeded?',
 			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
+				{ answerText: 'AWS Budgets', isCorrect: true },
+				{ answerText: 'AWS Cost Explorer', isCorrect: false },
+				{ answerText: 'AWS Cost Allocation Tags', isCorrect: false },
+				{ answerText: 'AWS Organizations', isCorrect: false },
 			],
 		},
+		{
+			questionText: 'Which design principle is achieved by following the reliability pillar of the AWS Well-Architected Framework?',
+			answerOptions: [
+				{ answerText: 'Vertical scaling', isCorrect: false },
+				{ answerText: 'Manual failure recovery', isCorrect: false },
+				{ answerText: 'Testing recovery procedures', isCorrect: true },
+				{ answerText: 'Changing infrastructure manually', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Performing operations as code is a design principle that supports which pillar of the AWS Well-Architected Framework?',
+			answerOptions: [
+				{ answerText: 'Performance efficiency', isCorrect: false },
+				{ answerText: 'Operational excellence', isCorrect: true },
+				{ answerText: 'Reliability', isCorrect: false },
+				{ answerText: 'Security', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which AWS networking service enables a company to create a virtual network within AWS?',
+			answerOptions: [
+				{ answerText: 'AWS Config', isCorrect: false },
+				{ answerText: 'Amazon Route 53', isCorrect: false },
+				{ answerText: 'AWS Direct Connect', isCorrect: false },
+				{ answerText: 'Amazon Virtual Private Cloud (Amazon VPC)', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Which of the following is an AWS responsibility under the AWS shared responsibility model?',
+			answerOptions: [
+				{ answerText: 'Configuring third-party applications', isCorrect: false },
+				{ answerText: 'Maintaining physical hardware', isCorrect: true },
+				{ answerText: 'Securing application access and data', isCorrect: false },
+				{ answerText: 'Managing guest operating systems', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which services can be used to deploy applications on AWS? (Choose TWO)',
+			answerOptions: [
+				{ answerText: 'AWS Elastic Beanstalk', isCorrect: true },
+				{ answerText: 'AWS Config', isCorrect: false },
+				{ answerText: 'AWS OpsWorks', isCorrect: true },
+				{ answerText: 'AWS Application Discovery Service', isCorrect: false },
+				{ answerText: 'Amazon Kinesis', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which component of the AWS global infrastructure does Amazon CloudFront use to ensure low-latency delivery?',
+			answerOptions: [
+				{ answerText: 'AWS Regions', isCorrect: false },
+				{ answerText: 'Edge Locations', isCorrect: true },
+				{ answerText: 'Availability Zones', isCorrect: false },
+				{ answerText: 'Virtual Private Cloud (VPC)', isCorrect: false },
+			],
+		},	
 	];
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
